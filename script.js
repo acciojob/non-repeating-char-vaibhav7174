7c98803637d5b5781594a99760b58ee4d4f8c070
Vaibhav Humbade <117819312+vaibhav7174@users.noreply.github.com>
2023-06-13 11:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+   let str=prompt("Enter a String");
+       let str1=str[nonreapting(str)]
+      function nonreapting(s){
+    for(i=0; i<s.length; i++)
+        if(s.indexOf(s[i])===s.lastIndexOf(s[i])) return i
+    return -1
+}
+//console.log(str1);
+alert(str1);
